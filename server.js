904dff70bb7ea995a7dd1c3b1112ec6e281c4ba1
ReticulatedSpline <carpenter.ben@icloud.com>
2020-10-01 07:34:39 -0500

    correct css mime type

--- server.js
+++ server.js
@@ -8,33 +8,33 @@ const page_mime = {'Content-Type': 'text/html' };
 const style_uri = 'resources/style.css';
 const style_mime = {'Content-Type': 'text/css' };
 
-function readFile(relativePath, fn) {
+function readFile(relativePath, mime, fn) {
 	var fullPath = path.join(__dirname, relativePath);
 	fs.readFile(fullPath, 'utf-8', 
 		function fileRead(err, text) {
-			fn(err, text);
+			fn(err, mime, text);
 		});
 	}
 
 var server = http.createServer(function (req, res) {
 
-	function respond(err, raw_content) {
+	function respond(err, mime, raw_content) {
 		if (err) {
-			res.writeHead(500, page_mime);
+			res.writeHead(500, mime);
 			console.log(err);
 			raw_content = bad_request;
 		} else {
-			res.writeHead(200, page_mime);
+			res.writeHead(200, mime);
 		}
 		res.end(raw_content);
 	}
 	
 	if (req.url == '/') {
-		readFile(page_uri, respond);
+		readFile(page_uri, page_mime, respond);
 	}
 	else if (req.url == '/style.css')
 	{
-		readFile(style_uri, respond);
+		readFile(style_uri, style_mime, respond);
 	}
 });
 
