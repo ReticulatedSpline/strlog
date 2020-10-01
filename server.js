e85dea9fb733239ba53b962a75bf61da6a3ac0ad
ReticulatedSpline <carpenter.ben@icloud.com>
2020-10-01 11:12:45 -0500

    added favicon

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const page_uri = 'resources/page.html';
 const page_mime = {'Content-Type': 'text/html' };
 const style_uri = 'resources/style.css';
 const style_mime = {'Content-Type': 'text/css' };
+const favicon_uri = 'resources/favicon.png';
+const favicon_mime = {'Content-Type': 'image/png' };
 
 function readFile(relativePath, mime, fn) {
 	var fullPath = path.join(__dirname, relativePath);
-	fs.readFile(fullPath, 'utf-8', 
-		function fileRead(err, text) {
+	fs.readFile(fullPath, function fileRead(err, text) {
 			fn(err, mime, text);
 		});
 	}
@@ -35,6 +36,10 @@ var server = http.createServer(function (req, res) {
 	else if (req.url == '/style.css')
 	{
 		readFile(style_uri, style_mime, respond);
+	}
+	else if (req.url == '/favicon.png')
+	{
+		readFile(favicon_uri, favicon_mime, respond);
 	}
 });
 
