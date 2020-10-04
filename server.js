24ecb084478bd66c8334f80155851697d9235b72
ReticulatedSpline <carpenter.ben@icloud.com>
2020-10-03 23:21:31 -0500

    parse markdown image links

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ function readFile(relativePath, mime, fn) {
 function formatPost(html, post) {
 	let content_regex = /{{content}}/;
 	post = html.replace(content_regex, post);
-	console.log("---1----")
-	console.log(post)
 	let title_regex = /#\s(.+)/g;
 	post = post.replace(title_regex, '<h1>$1</h1><br>');
-	console.log("---2----")
-	console.log(post)
+	let link_regex = /[^!]\[(.+)\]\((.+)\)/g;
+	post = post.replace(link_regex, '<a href="$2">$1</a>');
+	let image_regex = /!\[(.*)\]\((.+)\)/g;
+	post = post.replace(image_regex, '<br><br><img class="center" src="$2" alt="$1"><br>');
 	return post;
 }
 
