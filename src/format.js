56faf3a54b87dcc1b28299e6096655f170dce21d
ReticulatedSpline <carpenter.ben@icloud.com>
2020-10-04 18:37:34 -0500

    markdown unit tests

--- src/format.js
+++ src/format.js
@@ -1,13 +1,36 @@
-function formatPost (html, post) {
+function subContent (html, post) {
 	let content_regex = /{{content}}/;
-	post = html.replace(content_regex, post);
+	return html.replace(content_regex, post);
+}
+
+function subTitles(post) {
 	let title_regex = /#\s(.+)/g;
-	post = post.replace(title_regex, '<h1>$1</h1>');
+	return post.replace(title_regex, '<h1>$1</h1>');
+}
+
+function subLinks(post) {
 	let link_regex = /[^!]\[(.+?)\]\((.+?)\)/g;
-	post = post.replace(link_regex, ' <a href="$2">$1</a>');
+	return post.replace(link_regex, ' <a href="$2">$1</a>');
+}
+
+function subImages(post) {
 	let image_regex = /!\[(.*?)\]\((.+?)\)/g;
-	post = post.replace(image_regex, '<br><br><img class="center" src="$2" alt="$1"><br>');
+	let image_html = '<br><br><img class="center" src="$2" alt="$1"><br>';
+	return post.replace(image_regex, image_html);
+}
+
+function formatPost (html, post) {
+	post = subContent(html, post);
+	post = subTitles(post)
+	post = subLinks(post)
+	post = subImages(post)
 	return post;
 }
 
-module.exports.formatPost = formatPost;
+module.exports = {
+	subContent: subContent,
+	subTitles: subTitles,
+	subLinks: subLinks,
+	subImages: subImages,
+	formatPost: formatPost,
+};
--- test/test.js
+++ test/test.js
@@ -4,23 +4,54 @@ const ansi_green = "\x1b[42m";
 let passed = 0;
 let failed = 0;
 
-const format = require('./format.js');
+// tested modules
+const format = require('../src/format.js');
 
+// unit tests
+function testSubContent() {
+	let content = 'hello world';
+	let html = '<html><body>{{content}}</body></html>';
+	let actual = format.subContent(html, content);
+	let expected = '<html><body>hello world</body></html>';
+	runTest(testSubContent, expected, actual);
+}
+
+function testSubTitles() {
+	let post = '# title';
+	let actual = format.subTitles(post);
+	let expected = '<h1>title</h1>';
+	runTest(testSubTitles, expected, actual);
+}
+
+// testing framework
 tests = [
-	contentReplace,
+	testSubContent,
+	testSubTitles,
 ]
 
-for (let test of tests) {
-	if (test()) {
+function stripWhitespace(text) {
+	return text.replace(/\s+/g,'');
+}
+
+function runTest(test, expected, actual) {
+	// should whitespace be stripped in all cases?
+	expected = stripWhitespace(expected);
+	actual = stripWhitespace(actual);
+	if (expected === actual) {
 		passed++;
+		console.log('\t' + test.name + ' passed.');
+		return true;
 	}
 	else {
 		failed++;
+		console.log('\t' + test.name + ' failed.');
+		console.log('\t\tExpected: ' + expected + '\n\t\tActual:    ' + actual);
+		return false;
 	}
 }
 
-function contentReplace() {
-	let html = '<html><body>{{content}}';
+for (let test of tests) {
+	test();
 }
 
 let color = failed ? ansi_red_blink : ansi_green;
