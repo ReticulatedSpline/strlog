1a77db4f2403cc06c2d0f6349c4d3c65dd5ad808
Ben Carpenter <carpenter.ben@icloud.com>
2022-06-02 18:06:29 -0500

    update release branch

--- src/format.js
+++ src/format.js
@@ -1,214 +1,221 @@
-function subSidebar(html, post_data, make_translucent) {
+function insertSidebar(html, post_data, make_translucent) {
 	const list_regex = /{{sidebar}}/
-	const hostname = post_data.host
-	const post_list = post_data.previous_posts
-	const current_post = post_data.directory
-	let post_list_string = "<ul id=\"sidebar\">"
+	let post_list_string = '<ul id="sidebar">'
 	let opacity = 1
-	
-	for (let post_name of post_list) {
+
+	for (let post_name of post_data.previous_posts) {
 		if (opacity <= 0) {
-			break;
+			break
 		}
 		
-		let css = 'class = "sidebar_entry"';
-		let url = 'http://' + hostname + "/" + post_name;
+		let css = 'class = "tab'
+		let url = 'http://' + post_data.host + '/' + post_name
 
-		if (current_post == post_name) {
-			css += ' id = \"sidebar_current\" ';
+		if (post_data.no_url) {
+			url = ''
 		}
 
-		post_list_string += "<li style=\"opacity: " + opacity + "\">" +
-							"<a " + css + "href=" + url + ">" + post_name +
-							"</a></li>";
+		if (post_data.directory == post_name) {
+			css += ' current_tab"'
+		} else {
+			css += '"'
+		}
+
+		post_list_string += '<li style="opacity: ' + opacity + '">' +
+							'<a ' + css + 'href="' + url + '">' + post_name +
+							'</a></li>'
 
 		if (make_translucent) {
-			opacity -= 0.1;
-			opacity = opacity.toFixed(1);
+			opacity -= 0.1
+			opacity = opacity.toFixed(1)
 		}
 	}
 
-	post_list_string += "</ul>";
-	return html.replace(list_regex, post_list_string);
+	post_list_string += '</ul>'
+	return html.replace(list_regex, post_list_string)
 }
 
 function subTitles(post) {
-	post = post.replace(/^#{1}\s(.+)/gm, '<h1 class=\"h1\">$1</h1>\n');
-	post = post.replace(/^#{2}\s(.+)/gm, '<h2 class=\"h2\">$1</h2>\n');
-	post = post.replace(/^#{3}\s(.+)/gm, '<h3 class=\"h3\">$1</h3>\n');
-	post = post.replace(/^#{4}\s(.+)/gm, '<h4 class=\"h4\">$1</h4>\n');
-	post = post.replace(/^#{5}\s(.+)/gm, '<h5 class=\"h5\">$1</h5>\n');
-	return post.replace(/^#{6}\s(.+)/gm, '<h6 class=\"h6\">$1</h6>\n');
+	post = post.replace(/^#{1}\s(.+)/gm, '<h1 class=\"h1\">$1</h1>\n')
+	post = post.replace(/^#{2}\s(.+)/gm, '<h2 class=\"h2\">$1</h2>\n')
+	post = post.replace(/^#{3}\s(.+)/gm, '<h3 class=\"h3\">$1</h3>\n')
+	post = post.replace(/^#{4}\s(.+)/gm, '<h4 class=\"h4\">$1</h4>\n')
+	post = post.replace(/^#{5}\s(.+)/gm, '<h5 class=\"h5\">$1</h5>\n')
+	return post.replace(/^#{6}\s(.+)/gm, '<h6 class=\"h6\">$1</h6>\n')
 }
 
 function subLinks(post) {
-	const link_regex = /[^!]\[(.+?)\]\((.+?)\)/g;
-	return post.replace(link_regex, ' <a class=\"link body_link\" href="$2" target="_blank">$1</a>');
+	const link_regex = /[^!]\[(.+?)\]\((.+?)\)/g
+	return post.replace(link_regex, ' <a class="link body_link" href="$2" target="_blank">$1</a>')
 }
 
 function subImages(post, date) {
-	const image_regex = /!\[(.*?)\]\((.+?)\)/g;
+	const image_regex = /!\[(.*?)\]\((.+?)\)/g
 	const image_html =
 	'<div class="body_photo_div">' +
 	'<img class="body_photo" src="' + './posts/' + date + '/$2" alt="$1">' +
-	'<p class="body_alt_text">$1</p></div>';
-	return post.replace(image_regex, image_html);
+	'<p class="p body_alt_text">$1</p></div>'
+	return post.replace(image_regex, image_html)
 }
 
 function addParagraphs(post) {
-	const paragraph_regex = /(^[A-Za-z¿¡].*(?:\n[A-Za-z].*)*)/gm;
-	return post.replace(paragraph_regex, '<p class=\"p\">$1</p>');
+	const paragraph_regex = /(^[A-Za-z¿¡].*(?:\n[A-Za-z].*)*)/gm
+	return post.replace(paragraph_regex, '<p class="p">$1</p>')
 }
 
 function subRulers(post) {
-	const rule_regex = /\n---\n/g;
-	return post.replace(rule_regex, '\n<hr class=\"hr\">\n');
+	const rule_regex = /\n---\n/g
+	return post.replace(rule_regex, '\n<hr class="hr">\n')
 }
 
 function subStrikeTest(post) {
-	const strike_regex = /~~([^~]+?)~~/g;
+	const strike_regex = /~~([^~]+?)~~/g
 	return post.replace(strike_regex,
-		'<span style="text-decoration: line-through;">$1</span>');
+		'<span style="text-decoration: line-through">$1</span>')
 }
 
 function subBoldItalicText(post) {
-	const bold_italic_regex = /\*\*\*([^*]+)\*\*\*/g;
-	return post.replace(bold_italic_regex, '<strong class=\"strong\"><em>$1</em></strong>');
+	const bold_italic_regex = /\*\*\*([^*]+)\*\*\*/g
+	return post.replace(bold_italic_regex, '<strong class="strong"><em>$1</em></strong>')
 }
 
 function subBoldText(post) {
-	const bold_regex = /\*\*([^*]+)\*\*/g;
-	return post.replace(bold_regex, '<strong class=\"strong\">$1</strong>');
+	const bold_regex = /\*\*([^*]+)\*\*/g
+	return post.replace(bold_regex, '<strong class="strong">$1</strong>')
 }
 
 function subItalicText(post) {
-	const italic_regex1 = /\*([^*]+)\*/g;
-	const italic_regex2 = /\_([^_.]+)\_/g;
-	//post = post.replace(italic_regex1, '<em>$1</em>');
-	return post.replace(italic_regex1, '<em>$1</em>');
+	const italic_regex1 = /\*([^*]+)\*/g
+	const italic_regex2 = /\_([^_.]+)\_/g
+	//post = post.replace(italic_regex1, '<em>$1</em>')
+	return post.replace(italic_regex1, '<em>$1</em>')
 }
 
 function subBlockQuotes(post) {
-	const bold_regex = /^>\s([^\n]+)/gm;
-	return post.replace(bold_regex, '<p class="quote">$1</p>');
+	const bold_regex = /^>\s([^\n]+)/gm
+	return post.replace(bold_regex, '<p class="p quote">$1</p>')
 }
 
 function subPreText(post) {
 	// can't use backticks in code blocks with this regex
-	const pre_regex = /`([^`]+)`/g;
+	const pre_regex = /`([^`]+)`/g
 	const fence_regex = /```([^`]+)```/gm
-	post = post.replace(fence_regex, '<pre class=\"pre\">$1</pre>')
-	return post.replace(pre_regex, '<pre class=\"pre\">$1</pre>')
+	post = post.replace(fence_regex, '<pre class="p pre">$1</pre>')
+	return post.replace(pre_regex, '<pre class="p pre">$1</pre>')
 }
 
 function subUnorderedLists(post) {
-	const list_shell_regex = /((?:\n-\s.*)+)/gm;
-	const list_item_regex = /\n-\s([^\n]+)/m;
-	post = post.replace(list_shell_regex, '\n<ul class=\"ul\">$1\n</ul>');
+	const list_shell_regex = /((?:\n-\s.*)+)/gm
+	const list_item_regex = /\n-\s([^\n]+)/m
+	post = post.replace(list_shell_regex, '\n<ul class="p ul">$1\n</ul>')
 	while (results = post.match(list_item_regex)) {
 		post = post.replace(list_item_regex, '\n<li>$1</li>')
 	}
-	return post;
+	return post
 }
 
 function subOrderedLists(post) {
-	const list_shell_regex = /((?:\n\d\.\s.*)+)/gm;
-	const list_item_regex = /\n\d\.\s([^\n]+)/m;
-	post = post.replace(list_shell_regex, '\n<ol>$1\n</ol>');
+	const list_shell_regex = /((?:\n\d\.\s.*)+)/gm
+	const list_item_regex = /\n\d\.\s([^\n]+)/m
+	post = post.replace(list_shell_regex, '\n<ol class="p">$1\n</ol>')
 		while (results = post.match(list_item_regex)) {
 		post = post.replace(list_item_regex, '\n<li>$1</li>')
 	}
-	return post;
+	return post
 }
 
-function subFooter(post, lastPost, nextPost) {
-	const footer_regex = /{{footer}}/;
+function insertFooter(post, lastPost, nextPost) {
+	const footer_regex = /{{footer}}/
 
-	if (!lastPost || !nextPost) {
-		return post.replace(footer_regex, '');
+	if (!lastPost && !nextPost) {
+		return post.replace(footer_regex, '')
 	}
 
-	let footer = '<hr class="hr"/>';
+	let footer = '<hr class="hr"/>'
 	if (lastPost) {
-		footer = '<a id="last" href=' + lastPost + '>last</a>';
+		footer = '<a id="last" href="' + lastPost + '">last</a>'
 	} else {
-		footer = '<span class="grey" id="last">//last</span>';
+		footer = '<span class="grey" id="last">//last</span>'
 	}
 
 	if (nextPost) {
-		footer = footer + '<a id="next" href=' + nextPost + '>next</a>';
+		footer = footer + '<a id="next" href="' + nextPost + '">next</a>'
 	} else {
-		footer = footer + '<span class="grey" id="next">//next</span>';
+		footer = footer + '<span class="grey" id="next">//next</span>'
 	}
 
-	return post.replace(footer_regex, footer);
+	return post.replace(footer_regex, footer)
 }
 
-function subContent(post, post_data) {
+function insertContent(post, post_data) {
 	let body_text = '<div id="content">'
-		+ '<h1 class=\"h1\">'+ post_data.metadata.title + '</h1>'
-		+ "<div class=\"subtitle\">"
-		+ "<span class=\"p tagline\">" + post_data.metadata.tagline
-		+ "</span><div>"
+		+ '<h1 class="h1">'+ post_data.metadata.title + '</h1>'
+		+ '<div class="subtitle">'
+		+ '<span class="tagline">' + post_data.metadata.tagline
+		+ '</span><div class="topic_tags">'
 	for (topic of post_data.metadata.topics) {
 		let url = post_data.host + '/topics/' + topic
-		body_text += "<a href=\"http://" + url + "\" class=\"topics\">"
-				  + topic + "</a>"
+		body_text += '<a href="http://' + url + '" class="topic_tag">'
+				  + topic + '</a>'
 	}
-	body_text += "</div></div>"
+	body_text += '</div></div>'
 	body_text += post + '</div>'
-	return post_data.html.replace(/{{content}}/, body_text);
+	post_data.html = post_data.html.replace(/{{tagline}}/, '"'
+		+ post_data.metadata.tagline + '"')
+	post_data.html = post_data.html.replace(/{{title}}/, '"strlog: '
+		+ post_data.metadata.title + '"')
+	return post_data.html.replace(/{{content}}/, body_text)
 }
 
 function addEndMark(post) {
-	return post + ' ▣';
+	return post + ' ▣'
 }
 
 function stripCarriageReturns(post) {
-	return post.replace(/\r/g, '');
+	return post.replace(/\r/g, '')
 }
 
-function subHeader(post, post_data) {
+function insertHeader(post, post_data) {
 	let header = '<ul id="main_menu">'
-	let tabs = ['recent', 'topics', 'about']
+	let tabs = ['posts', 'topics', 'about']
 	let urls = ['/', '/topics', '/about']
 	let index = 0
 	while (index < 3) {
+		header += '<li>'
 		if (post_data.current_tab == tabs[index]) {
-			header += '<li class="h1 strong current_tab">'
+			header += '<a class="h1 tab current_tab" '
 		} else {
-			header += '<li class="h1 strong">'
+			header += '<a class="h1 tab" '
 		}
-		header += '<a href="' + urls[index] + '">'
+		header += 'href="' + urls[index] + '">'
 		header += tabs[index] + '</a></li>'
-		header += '</a></li>'
 		index += 1
 	}
 	return post.replace(/{{header}}/, header)
 }
 
 function formatPost (post_data, fn) {
-	let post = post_data.markdown;
-	post = stripCarriageReturns(post);
-	post = subTitles(post);
-	post = subLinks(post);
-	post = subImages(post, post_data.directory);
-	post = subPreText(post);
-	post = addEndMark(post);
-	post = addParagraphs(post);
-	post = subBoldItalicText(post);
-	post = subBoldText(post);
-	post = subStrikeTest(post);
-	post = subItalicText(post);
-	post = subBlockQuotes(post);
-	post = subUnorderedLists(post);
-	post = subOrderedLists(post);
-	post = subRulers(post);
-	post = subContent(post, post_data);
-	post = subSidebar(post, post_data, true);
-	post = subHeader(post, post_data)
-	post = subFooter(post, post_data.last_post_url, post_data.next_post_url);
-	fn(post);
+	let post = post_data.markdown
+	post = stripCarriageReturns(post)
+	post = subTitles(post)
+	post = subLinks(post)
+	post = subImages(post, post_data.directory)
+	post = subPreText(post)
+	post = addEndMark(post)
+	post = addParagraphs(post)
+	post = subBoldItalicText(post)
+	post = subBoldText(post)
+	post = subStrikeTest(post)
+	post = subItalicText(post)
+	post = subBlockQuotes(post)
+	post = subUnorderedLists(post)
+	post = subOrderedLists(post)
+	post = subRulers(post)
+	post = insertContent(post, post_data)
+	post = insertSidebar(post, post_data, true)
+	post = insertHeader(post, post_data)
+	post = insertFooter(post, post_data.last_post_url, post_data.next_post_url)
+	fn(post)
 }
 
 function concatTitles(page_data) {
@@ -223,42 +230,39 @@ function concatTitles(page_data) {
 		content += '</h3>'
 		content += '</br>'
 	}
-	content += "</div>"
-
-	page_data.html = page_data.html.replace(/{{content}}/, content);
+	content += '</div>'
+	content = subItalicText(content)
+	page_data.html = page_data.html.replace(/{{content}}/, content)
 }
 
-function subTopicSidebar(page_data) {
-	let sidebar_string = "<ul id=\"sidebar\">"
-
+function insertTopicSidebar(page_data) {
+	let sidebar_string = '<ul id=\"sidebar\">'
 	for (topic in page_data.topics) {
-		let css = 'class = \"sidebar_entry\" '
-		let url = ''
-		if (!url.includes('topics/')) {
-			url += 'topics/' + topic
-		} else {
-			url += topic
-		}
+		let css = 'class = "tab '
+		let url = 'http://' + page_data.host + '/topics/' + topic
+
 		if (page_data.current_topic == topic) {
-			css += 'id = \"sidebar_current\" '
+			css += 'current_tab"'
+		} else {
+			css += '"'
 		}
 
-		sidebar_string += "<li><a " + css + 'href="' + url + '">' + topic +"</a></li>";
+		sidebar_string += '<li><a ' + css + 'href="' + url + '">' + topic +'</a></li>'
 	}
 
-	sidebar_string += "</ul>";
-	return page_data.html.replace(/{{sidebar}}/, sidebar_string);
+	sidebar_string += '</ul>'
+	return page_data.html.replace(/{{sidebar}}/, sidebar_string)
 }
 
 function formatTopic(page_data, fn) {
-	concatTitles(page_data);
-	page_data.html = subHeader(page_data.html, page_data)
-	page_data.html = subFooter(page_data.html, null, null)
-	page_data.html = subTopicSidebar(page_data);
-	fn(page_data.html);
+	concatTitles(page_data)
+	page_data.html = insertHeader(page_data.html, page_data)
+	page_data.html = insertFooter(page_data.html, null, null)
+	page_data.html = insertTopicSidebar(page_data)
+	fn(page_data.html)
 }
 
 module.exports = {
 	formatPost: formatPost,
 	formatTopic: formatTopic
-};
+}
--- src/server.js
+++ src/server.js
@@ -1,122 +1,182 @@
-const fs = require('fs');
-const http = require('http');
-const path = require('path');
-const { formatPost, formatTopic } = require('./format.js');
+const { dir } = require('console')
+const fs = require('fs')
+const http = require('http')
+const path = require('path')
+const { formatPost, formatTopic } = require('./format.js')
 
-const CSS_MIME  = { 'Content-Type': 'text/css' };
-const HTML_MIME = { 'Content-Type': 'text/html' };
-const ICO_MIME  = { 'Content-Type': 'image/x-icon' };
-const JPG_MIME  = { 'Content-Type': 'image/jpg' };
-const PNG_MIME  = { 'Content-Type': 'image/png' };
-const appRoot = path.join(__dirname, '../');
-const html_path = path.join(appRoot, './resources', 'page.html')
-const port = process.env.PORT || 5000;
+const CSS_MIME  = { 'Content-Type': 'text/css' }
+const HTML_MIME = { 'Content-Type': 'text/html' }
+const ICO_MIME  = { 'Content-Type': 'image/x-icon' }
+const JPG_MIME  = { 'Content-Type': 'image/jpg' }
+const PNG_MIME  = { 'Content-Type': 'image/png' }
+const app_root = path.join(__dirname, '../')
+const html_path = 'resources/page.html'
+const error_path = 'resources/error.html'
+
+const port = process.env.PORT || 5000
+//var posts = cacheAllPosts();
+//console.log('Loaded', posts.length, 'posts.')
+
+function sortByISODate(a, b) {
+	let options = {numeric: true, sensitivity: 'base'}
+	return b.localeCompare(a, undefined, options)
+}
+
+function cacheAllPosts() {
+	let post_dirs = fs.readdirSync(path.join(app_root, 'posts'))
+	// natural sort file names (should be ISO dates!)
+	post_dirs.sort(sortByISODate)
+	// drop hidden dir names, alphabetic dir names
+	post_dirs = post_dirs.filter(item=> !/^[A-z\.].*/.test(item))
+	let index = 0
+	let posts = []
+	for (directory in post_dirs) {
+		let post = {
+
+		}
+		index += 1
+		posts.push(post)
+	}
+	return post_dirs
+}
 
 let server = http.createServer(function (req, res) {
-	const host = req.headers.host;
+	const host = req.headers.host
 
 	if (req.url == '/') {
 		routeMostRecentPost(res)
 	}
 	else if (req.url.match(/\d{4}-\d{2}-\d{2}$/)) {
-		routeSpecificPost(host, req.url, res);
+		routeSpecificPost(host, req.url, res)
 	}
 	else if (req.url.endsWith('/topics')) {
-		routeTopics(req, res)
+		routeAllTopics(req, res)
 	}
 	else if (req.url.match(/\/topics\/[A-z]+$/)) {
 		routeSpecificTopic(req, res)
 	}
-	else if (req.url.startsWith('/about')) {
-		//routeAbout(req, res)
-		routeError(req, res)
+	else if (req.url.endsWith('/about')) {
+		routeAbout(req, res)
 	}
 	else if (req.url.endsWith('/style.css')) {
-		let uri = path.join(appRoot, 'resources', 'style.css');
+		let uri = path.join(app_root, 'resources', 'style.css')
 		routeCSS(uri, res)
 	}
 	else if (req.url == '/favicon.ico') {
-		let uri = path.join(appRoot, 'resources', 'favicon.ico');
+		let uri = path.join(app_root, 'resources', 'favicon.ico')
 		routeImage(uri, ICO_MIME, res)
 	}
 	else if (req.url == '/icon.png') {
-		let uri = path.join(appRoot, 'resources', 'icon.png');
+		let uri = path.join(app_root, 'resources', 'icon.png')
 		routeImage(uri, PNG_MIME, res)
 	}
 	else if (req.url.endsWith('.jpg') || req.url.endsWith('.jpeg')) {
-		let uri = path.join(appRoot, req.url);
+		let uri = path.join(app_root, req.url)
 		routeImage(uri, JPG_MIME, res)
 	}
 	else if (req.url.endsWith('.png')) {
-		let uri = path.join(appRoot, req.url);
+		let uri = path.join(app_root, req.url)
 		routeImage(uri, PNG_MIME, res)
 	}
 	else {
 		routeError(req, res)
 	}
-});
+})
 
 function routeCSS(uri, res) {
 	fs.readFile(uri, 'utf8', (err, data) => {
-		sendContent(data, CSS_MIME, res);
-	});
+		sendContent(data, CSS_MIME, res)
+	})
 }
 
 function routeImage(uri, mimetype, res) {
 	fs.readFile(uri, (err, data) => {
-		sendContent(data, mimetype, res);
-	});
+		sendContent(data, mimetype, res)
+	})
 }
 
 function routeMostRecentPost(res) {
-	console.log("Redirecting to most recent post")
+	console.log('Redirecting to most recent post')
 	getAllPostsByDate((files) => {
-		res.writeHead(302, {'Location': files[0]});
-		res.end();
-	});
+		res.writeHead(302, {'Location': files[0]})
+		res.end()
+	})
 }
 
 function routeSpecificPost(host, url, res) {
 	getAllPostsByDate((files) => {
 		// check for matching post
-		let post_index = 0;
+		let post_index = 0
 		for (const [index, post] of files.entries()) {
 			if (post === url.substring(1)) {
-				console.log("Post", post, "requested");
-				post_index = index;
+				console.log('Post', post, 'requested')
+				post_index = index
 			}
 		}
 	
 		const post_dir = files[post_index]
 
-		let next_post_dir
-		if (post_index + 1 < files.length) {
+		let next_post_dir = null
+		if (post_index > 0) {
 			next_post_dir = files[post_index + 1]
+		}
+
+		let last_post_dir = null
+		if (post_index < files.length) {
+			last_post_dir = files[post_index - 1]
 		}
 
 		let previous_posts = files.slice(0, 10)
 
-		buildPostResponse(host, post_dir, previous_posts, next_post_dir, (postContent) => {
-			sendContent(postContent, HTML_MIME, res);
-		});
+		buildPostResponse(host, post_dir, previous_posts, last_post_dir, next_post_dir, (postContent) => {
+			sendContent(postContent, HTML_MIME, res)
+		})
 	})
 }
 
 function routeAbout(req, res) {
-	//send about page
+	console.log('About page requested.')
+	fs.readFile('./posts/about/post.md', 'utf8', (err, markdown) => {
+		if (err) {
+			console.log('Error reading ./posts/about:', error)
+			return
+		}
+		let index = parseInt(Math.random() * 10) + 3
+		let post_list = []
+		while (index > 0) {
+			post_list.push('▒'.repeat(parseInt(Math.random() * 10)))
+			index -= 1
+		}
+
+		fs.readFile(html_path, 'utf8', (err, html) => {
+			let metadata_path = path.join(app_root, './posts/about/metadata.json')
+			let page_data = {
+				html: html,
+				host: req.headers.host,
+				directory: 'about',
+				markdown: markdown,
+				metadata: require(metadata_path),
+				previous_posts: post_list,
+				current_tab: 'about',
+				no_url: true
+			}
+			let fn = (page) => {sendContent(page, HTML_MIME, res)}
+			formatPost(page_data, fn)
+		})
+	})
 }
 
 function routeError(req, res) {
-	console.log("Invalid route " + req.url)
-	fs.readFile('resources/error.html', 'utf8', (err, html) => {
+	console.log('Invalid route ' + req.url)
+	fs.readFile(error_path, 'utf8', (err, html) => {
 		sendContent(html, HTML_MIME, res)
 	})
 }
 
 function getAllPostsByDate(fn) {
-	fs.readdir(path.join(appRoot, 'posts'), (err, files) => {
+	fs.readdir(path.join(app_root, 'posts'), (err, files) => {
 		if (err) {
-			console.log('Error fetching files: ', err);
+			console.log('Error fetching files: ', err)
 		}
 		else {
 			// natural sort file names (should be ISO dates!)
@@ -124,27 +184,27 @@ function getAllPostsByDate(fn) {
 				return b.localeCompare(a, undefined, {
 					numeric: true,
 					sensitivity: 'base'
-				});
-			});
-			// drop hidden files like .DS_Store
-			files = files.filter(item=> !/^\..*/.test(item))
-			fn(files);
+				})
+			})
+			// drop hidden dir names, alphabetic dir names
+			files = files.filter(item=> !/^[A-z\.].*/.test(item))
+			fn(files)
 		}
-	});
+	})
 }
 
 function getMetadataFiles(dir, files_) {
-	files_ = files_ || [];
-	var files = fs.readdirSync(dir);
+	files_ = files_ || []
+	var files = fs.readdirSync(dir)
 	for (var i in files){
-		var name = dir + '/' + files[i];
+		var name = dir + '/' + files[i]
 		if (fs.statSync(name).isDirectory()) {
-			getMetadataFiles(name, files_);
+			getMetadataFiles(name, files_)
 		} else if (name.endsWith('metadata.json')) {
-			files_.push(name);
+			files_.push(name)
 		}
 	}
-	return files_;
+	return files_
 }
 
 function routeSpecificTopic(req, res) {
@@ -152,9 +212,9 @@ function routeSpecificTopic(req, res) {
 	let current_topic = {}
 	let url_parts = req.url.split('/')
 	let topic_name = url_parts[url_parts.length - 1]
-	console.log('Requested topic', topic)
+	console.log('Requested topic', topic_name)
 	for (file of getMetadataFiles('./posts')) {
-		metadata = require(path.join(appRoot, file))
+		metadata = require(path.join(app_root, file))
 		for (topic of metadata.topics) {
 			// gross hack to grab the date string out of the path :(
 			let directory = file.split('/')[2]
@@ -175,25 +235,15 @@ function routeSpecificTopic(req, res) {
 		}
 	}
 
-	fs.readFile(html_path, 'utf8', (err, html) => {
-		page_data = {
-			html: html,
-			host: req.headers.host,
-			topics: topics,
-			current_topic: current_topic,
-			current_tab: 'topics'
-		}
-		let fn = (postContent) => {sendContent(postContent, HTML_MIME, res)}
-		formatTopic(page_data, fn)
-	})
+	buildTopicResponse(req, res, topics, current_topic)
 }
 
-function routeTopics(req, res) {
+function routeAllTopics(req, res) {
 	let topics = {}
 	let current_topic = {}
 	console.log('Topic list requested')
 	for (file of getMetadataFiles('./posts')) {
-		metadata = require(path.join(appRoot, file))
+		metadata = require(path.join(app_root, file))
 		for (topic of metadata.topics) {
 			// gross hack to grab the date string out of the path :(
 			let directory = file.split('/')[2]
@@ -213,6 +263,10 @@ function routeTopics(req, res) {
 		}
 	}
 
+	buildTopicResponse(req, res, topics, current_topic)
+}
+
+function buildTopicResponse(req, res, topics, current_topic) {
 	fs.readFile(html_path, 'utf8', (err, html) => {
 		page_data = {
 			html: html,
@@ -226,34 +280,34 @@ function routeTopics(req, res) {
 	})
 }
 
-function buildPostResponse(host, post_dir, previous_posts, next_post, fn) {
-	const metadata_path = path.join(appRoot, './posts', post_dir, 'metadata.json')
-	const markdown_path = path.join(appRoot, './posts', post_dir, 'post.md')
+function buildPostResponse(host, post_dir, previous_posts, next_post, last_post, fn) {
+	const metadata_path = path.join(app_root, './posts', post_dir, 'metadata.json')
+	const markdown_path = path.join(app_root, './posts', post_dir, 'post.md')
 
 	let post_data = {
 		host: host,
 		directory: post_dir,
 		previous_posts: previous_posts,
-		last_post_url: previous_posts[0],
+		last_post_url: last_post,
 		next_post_url: next_post,
 		html_dir: html_path,
 		metadata: require(metadata_path),
-		current_tab: 'recent'
+		current_tab: 'posts'
 	}
 
 	fs.readFile(html_path, 'utf8', (err, html) => {
-		post_data.html = html;
+		post_data.html = html
 		fs.readFile(markdown_path, 'utf8', (err, markdown) => {
-			post_data.markdown = markdown;
-			formatPost(post_data, fn);
-		});
-	});
+			post_data.markdown = markdown
+			formatPost(post_data, fn)
+		})
+	})
 }
 
 function sendContent(content, mime, res) {
-	res.writeHead(200, mime);
-	res.end(content);
+	res.writeHead(200, mime)
+	res.end(content)
 }
 
-server.listen(port);
-console.log('Server online and listening at port ' + port);
+server.listen(port)
+console.log('Server online and listening at port ' + port)
