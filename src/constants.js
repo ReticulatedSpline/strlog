d510f285a0bf6c99c23b952c6c0875c7cc3bd48f
Ben Carpenter <carpenter.ben@icloud.com>
2025-04-21 08:09:54 -0500

    disable overflow-x on mobile

--- src/constants.js
+++ src/constants.js
@@ -64,5 +64,8 @@ global.WIKI_LINKS = [
     "https://en.m.wikipedia.org/wiki/Don_Norman",
     "https://en.wikipedia.org/wiki/Hannibal_Directive",
     "https://en.wikipedia.org/wiki/Dahiya_doctrine",
-    "https://en.wikipedia.org/wiki/Out-of-place_artifact"
+    "https://en.wikipedia.org/wiki/Out-of-place_artifact",
+    "https://en.wikipedia.org/wiki/Long-term_nuclear_waste_warning_messages",
+    "https://en.wikipedia.org/wiki/Tsunami_stone"
+
 ];
